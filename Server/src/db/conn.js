27967373d20cffef94d2a651b0fe20652ad6d516
Ankita <ankitasrivastava.stim@gmail.com>
2024-09-05 14:53:42 +0530

    Add env and connect with mongoatlus

--- Server/src/db/conn.js
+++ Server/src/db/conn.js
@@ -1,8 +1,23 @@
 const mongoose= require ("mongoose");
+require("dotenv").config(); 
+const uri = process.env.MONGO_URI;
+if (!uri) {
+    console.error('MongoDB connection URI is missing!');
+   
+  }
 
-mongoose.connect("mongodb://localhost:27017/course-api", {
-}).then(()=>{
-    console.log("connection successfull");
-}).catch((e)=>{
-    console.log("no connection");
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    
+    useUnifiedTopology: true,
+   
 })
+.then(() => console.log("MongoDB Atlas connected successfully"))
+.catch((error) => console.error("mongoDBAtlus connection error : ", error));
+
+// mongoose.connect("mongodb://localhost:27017/course-api", {
+// }).then(()=>{
+//     console.log("connection successfull");
+// }).catch((e)=>{
+//     console.log("no connection");
+// })
--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,6 @@
-const express= require ("express")
+const express= require("express")
 const cors =require("cors");
 const bcrypt = require('bcrypt');
-
 require("./db/conn");
 const StudentModel=require("./models/Student");
 const Course= require("./models/course");
